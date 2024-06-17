6d5d78d83603b3a5b1e41ef34facb1cc4dcbc63d
caiociribeiro <devcaiociriaco@gmail.com>
2024-06-17 16:23:46 -0300

    reworked css

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const grid = document.querySelector('.board');
-const reset = document.querySelector('.reset');
+const grid = document.querySelector(".board");
+const reset = document.querySelector(".reset");
 const root = document.documentElement;
-const dificulty = document.querySelectorAll('option');
-const select = document.querySelector('select');
-const endGame = document.querySelector('.end-game');
-const playAgain = document.getElementById('play--again');
+const dificulty = document.querySelectorAll("option");
+const select = document.querySelector("select");
+const endGame = document.querySelector(".end-game");
+const playAgain = document.getElementById("play--again");
 let rows = 14;
 let cols = 18;
 let bombs = 40;
@@ -41,8 +41,8 @@ function stopWatch() {
       secString = `0${secString}`;
     }
 
-    document.getElementById('min').innerHTML = minString;
-    document.getElementById('sec').innerHTML = secString;
+    document.getElementById("min").innerHTML = minString;
+    document.getElementById("sec").innerHTML = secString;
     setTimeout(stopWatch, 10);
   }
 }
@@ -52,8 +52,8 @@ function stopStopWatch() {
   minute = 0;
   second = 0;
   count = 0;
-  document.getElementById('min').innerHTML = '00:';
-  document.getElementById('sec').innerHTML = '00';
+  document.getElementById("min").innerHTML = "00:";
+  document.getElementById("sec").innerHTML = "00";
 }
 
 function getRandomInt(min, max) {
@@ -74,21 +74,21 @@ function drawTiles() {
     cols = 24;
     bombs = 99;
   }
-  root.style.setProperty('--col', cols);
-  root.style.setProperty('--width', cols * 31);
-  root.style.setProperty('--height', rows * 31);
+  root.style.setProperty("--col", cols);
+  root.style.setProperty("--width", cols * 31);
+  root.style.setProperty("--height", rows * 31);
   const tiles = [];
   let id = 0;
   for (let i = 0; i < rows; i++) {
     tiles.push([]);
     for (let j = 0; j < cols; j++) {
-      const tile = document.createElement('button');
+      const tile = document.createElement("button");
       tile.disabled = false;
-      tile.classList.add('tile');
-      tile.classList.add('begin');
+      tile.classList.add("tile");
+      tile.classList.add("begin");
       tile.classList.add(`c${j}`);
       tile.classList.add(`r${i}`);
-      tile.setAttribute('oncontextmenu', 'return false;');
+      tile.setAttribute("oncontextmenu", "return false;");
       grid.appendChild(tile);
       tiles[i].push({
         id: id,
@@ -221,12 +221,12 @@ function showTiles(x, y) {
   checkedTiles.push(tiles[x][y].id);
   if (!tiles[x][y].blank) {
     tiles[x][y].tile.innerHTML = tiles[x][y].bombsAround;
-    tiles[x][y].tile.setAttribute('displayed', true);
+    tiles[x][y].tile.setAttribute("displayed", true);
     tiles[x][y].tile.disabled = true;
 
     return;
   }
-  tiles[x][y].tile.setAttribute('displayed', true);
+  tiles[x][y].tile.setAttribute("displayed", true);
   tiles[x][y].tile.disabled = true;
   if (y + 1 < cols) showTiles(x, y + 1);
   if (y - 1 >= 0) showTiles(x, y - 1);
@@ -241,14 +241,14 @@ function showTiles(x, y) {
 function loadBoard() {
   tiles.forEach((col) => {
     col.forEach((tile) => {
-      tile.tile.addEventListener('mouseup', (e) => {
+      tile.tile.addEventListener("mouseup", (e) => {
         e.preventDefault();
         if (e.button === 0) {
-          if (tile.tile.classList.contains('begin')) {
+          if (tile.tile.classList.contains("begin")) {
             handleFirstClick(tile);
             tiles.forEach((col) => {
               col.forEach((tile) => {
-                tile.tile.classList.remove('begin');
+                tile.tile.classList.remove("begin");
               });
             });
           } else {
@@ -268,7 +268,7 @@ function resetBoard() {
   loadBoard();
   tiles.forEach((col) => {
     col.forEach((tile) => {
-      tile.tile.classList.add('begin');
+      tile.tile.classList.add("begin");
     });
   });
   stopStopWatch();
@@ -286,20 +286,19 @@ function handleFirstClick(tile) {
 }
 
 function showBombs() {
-  let delay = 100;
+  let delay = 0;
   for (let i = 0; i < tiles.length; i++) {
     for (let j = 0; j < tiles[i].length; j++) {
       tiles[i][j].tile.disabled = true;
     }
   }
-  // alert('game over');
   for (let i = 0; i < tiles.length; i++) {
     for (let j = 0; j < tiles[i].length; j++) {
       setTimeout(() => {
         if (tiles[i][j].bomb) {
           if (tiles[i][j].tile.firstChild) tiles[i][j].tile.firstChild.remove();
           const img = new Image();
-          img.src = './images/bomb.png';
+          img.src = "./images/bomb.png";
           tiles[i][j].tile.appendChild(img);
         }
       }, delay);
@@ -320,7 +319,7 @@ function handleClick(tile) {
     if (tile.blank) {
       showTiles(tile.y, tile.x);
     } else {
-      tile.tile.setAttribute('displayed', 'true');
+      tile.tile.setAttribute("displayed", "true");
       tile.tile.innerHTML = tile.bombsAround;
       tile.tile.disabled = true;
     }
@@ -346,7 +345,7 @@ function handleClick(tile) {
 function handleFlagClick(tile) {
   if (!tile.tile.firstChild) {
     const img = new Image();
-    img.src = './images/flag.png';
+    img.src = "./images/flag.png";
     tile.tile.appendChild(img);
   } else {
     tile.tile.firstChild.remove();
@@ -354,8 +353,8 @@ function handleFlagClick(tile) {
 }
 
 function hideEndGameMenu() {
-  endGame.style.zIndex = '0';
-  grid.style.filter = 'blur(0px)';
+  endGame.classList.remove("end-game-show");
+  grid.style.filter = "blur(0px)";
   playAgain.disabled = true;
 }
 
@@ -372,28 +371,28 @@ function displayEndGameMenu(result, sec, min) {
   }
   const time = `Time: ${minString}:${secString}`;
 
-  const endTime = document.getElementById('end--time');
-  const resultDisplay = document.getElementById('result--display');
+  const endTime = document.getElementById("end--time");
+  const resultDisplay = document.getElementById("result--display");
 
-  resultDisplay.innerHTML = result === 0 ? 'YOU WON' : 'GAME OVER';
+  resultDisplay.innerHTML = result === 0 ? "YOU WON" : "GAME OVER";
 
   endTime.innerHTML = time;
-  endGame.style.zIndex = '1';
-  grid.style.filter = 'blur(5px)';
+  endGame.classList.add("end-game-show");
+  grid.style.filter = "blur(5px)";
   playAgain.disabled = false;
 }
 
-reset.addEventListener('click', (e) => {
+reset.addEventListener("click", (e) => {
   resetBoard();
   hideEndGameMenu();
 });
 
-select.addEventListener('change', (e) => {
+select.addEventListener("change", (e) => {
   resetBoard();
   hideEndGameMenu();
 });
 
-playAgain.addEventListener('click', (e) => {
+playAgain.addEventListener("click", (e) => {
   resetBoard();
   hideEndGameMenu();
 });
